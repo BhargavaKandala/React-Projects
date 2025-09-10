43af820ae00ee240e0fc1a5d0a7b9b11aac8c7fc
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2025-09-10 11:30:28 +0530

    Practise

--- daily-thub14/src/App.jsx
+++ daily-thub14/src/App.jsx
@@ -1,19 +1,26 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import MyImage from './assets/react.svg';
+import CardComponent from './Myfunction.jsx';
 import './App.css'
-import MyButton from './Mybutton.jsx';
-import ListRendering from './ListRendering.jsx';
-import Myfunction from './Myfunction.jsx';
 
-//Responding to events
 function App() {
-  
-  return (
-    <>
-      {/* <MyButton/>
-      <MyButton/>
-      <ListRendering/> */}
-        <Myfunction/>
-    </>
-  )
+  var MyList = [
+        { "Image":MyImage, "Price":"2999", "Brand":"Ajio" },
+        { "Image":MyImage, "Price":"2795", "Brand":"Rare Rabbit" },
+        { "Image":MyImage, "Price":"5390", "Brand":"Puma" },
+        { "Image":MyImage, "Price":"7800", "Brand":"Adidas" }
+    ]
+
+    return(
+      <div className="parent">
+        {
+          MyList.map(ele=>{
+            return <CardComponent Data = {ele}/>
+          })
+        }
+      </div>
+    )
 }
 
 export default App;
--- daily-thub14/src/Myfunction.jsx
+++ daily-thub14/src/Myfunction.jsx
@@ -1,34 +1,15 @@
-import React from 'react';
-
-export default function Myfunction() {
-    const listItems = [
-        {
-            "id": 1,
-            "brand": "USPA",
-            "Price": 2500
-        }, {
-            "id": 2,
-            "brand": "Ajio",
-            "Price": 3200
-        }, {
-            "id": 3,
-            "brand": "Rare Rabbit",
-            "Price": 5400
-        }
-    ];
-
+import React from "react";
+const Myfunction = (Props) => {
+    
     return (
         <>
-            <ol>
-                {
-                    listItems.map((ele) => (
-                        <li key={ele.id}>
-                            <h1>Price: {ele.Price}</h1>
-                            Brand: {ele.brand}
-                        </li>
-                    ))
-                }
-            </ol>
+            <div className="card">
+                <div className="info">
+                    <div className="price">Price: {Props.Data.Price}/-</div>
+                    <div className="brand">Brand: {Props.Data.Brand}</div>
+                </div>
+            </div>
         </>
     );
-}
+};
+export default Myfunction;
